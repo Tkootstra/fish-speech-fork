23b15dc78b93d6a53a7fd4174cc8417832308477
Timo Kootstra <timokootstr@msn.com>
2025-04-28 17:10:27 +0200

    standalone python interface

--- fish_speech/inference_engine/__init__.py
+++ fish_speech/inference_engine/__init__.py
@@ -1,11 +1,9 @@
 import gc
 import queue
 from typing import Generator
-
 import numpy as np
 import torch
 from loguru import logger
-
 from fish_speech.inference_engine.reference_loader import ReferenceLoader
 from fish_speech.inference_engine.utils import InferenceResult, wav_chunk_header
 from fish_speech.inference_engine.vq_manager import VQManager
@@ -15,12 +13,12 @@ from fish_speech.models.text2semantic.inference import (
     WrappedGenerateResponse,
 )
 from fish_speech.models.vqgan.modules.firefly import FireflyArchitecture
+from fish_speech.text.chn_text_norm.text import Text as ChnNormedText
 from fish_speech.utils import autocast_exclude_mps, set_seed
 from fish_speech.utils.schema import ServeTTSRequest
 
 
 class TTSInferenceEngine(ReferenceLoader, VQManager):
-
     def __init__(
         self,
         llama_queue: queue.Queue,
@@ -28,14 +26,11 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
         precision: torch.dtype,
         compile: bool,
     ) -> None:
-
         super().__init__()
-
         self.llama_queue = llama_queue
         self.decoder_model = decoder_model
         self.precision = precision
         self.compile = compile
-
     @torch.inference_mode()
     def inference(self, req: ServeTTSRequest) -> Generator[InferenceResult, None, None]:
         """
@@ -48,6 +43,7 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
         ref_id: str | None = req.reference_id
         prompt_tokens, prompt_texts = [], []
         # Load the reference audio and text based on id or hash
+        # Load the reference audio and text based on id, hash, or preprocessed references
         if ref_id is not None:
             prompt_tokens, prompt_texts = self.load_by_id(ref_id, req.use_memory_cache)
 
@@ -56,17 +52,18 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
                 req.references, req.use_memory_cache
             )
 
+        elif req.preprocessed_references:
+            prompt_tokens = [ref.tokens for ref in req.preprocessed_references]
+            prompt_texts = [ref.text for ref in req.preprocessed_references]
+
         # Set the random seed if provided
         if req.seed is not None:
             set_seed(req.seed)
             logger.warning(f"set seed: {req.seed}")
-
         # Get the symbolic tokens from the LLAMA model
         response_queue = self.send_Llama_request(req, prompt_tokens, prompt_texts)
-
         # Get the sample rate from the decoder model
         sample_rate = self.decoder_model.spec_transform.sample_rate
-
         # If streaming, send the header
         if req.streaming:
             yield InferenceResult(
@@ -77,9 +74,7 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
                 ),
                 error=None,
             )
-
         segments = []
-
         while True:
             # Get the response from the LLAMA model
             wrapped_result: WrappedGenerateResponse = response_queue.get()
@@ -94,18 +89,17 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
                     ),
                 )
                 break
-
             # Check the response type
             if not isinstance(wrapped_result.response, GenerateResponse):
                 raise TypeError(
                     "Expected GenerateResponse, got {type(wrapped_result.response).__name__}"
                 )
-
             result: GenerateResponse = wrapped_result.response
             if result.action != "next":
                 segment = self.get_audio_segment(result)
 
                 if req.streaming:  # Used only by the API server
+                if req.streaming:
                     yield InferenceResult(
                         code="segment",
                         audio=(sample_rate, segment),
@@ -114,12 +108,10 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
                 segments.append(segment)
             else:
                 break
-
         # Clean up the memory
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
             gc.collect()
-
         # Edge case: no audio generated
         if len(segments) == 0:
             yield InferenceResult(
@@ -135,21 +127,22 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
                 audio=(sample_rate, audio),
                 error=None,
             )
-
         return None
-
     def send_Llama_request(
         self, req: ServeTTSRequest, prompt_tokens: list, prompt_texts: list
     ) -> queue.Queue:
         """
         Send a request to the LLAMA model to generate the symbolic tokens.
         """
-
         # Prepare the request
         request = dict(
             device=self.decoder_model.device,
             max_new_tokens=req.max_new_tokens,
-            text=req.text,
+            text=(
+                req.text
+                if not req.normalize
+                else ChnNormedText(raw_text=req.text).normalize()
+            ),
             top_p=req.top_p,
             repetition_penalty=req.repetition_penalty,
             temperature=req.temperature,
@@ -160,10 +153,8 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
             prompt_tokens=prompt_tokens,
             prompt_text=prompt_texts,
         )
-
         # Create a queue to get the response
         response_queue = queue.Queue()
-
         # Send the request to the LLAMA model
         self.llama_queue.put(
             GenerateRequest(
@@ -171,20 +162,16 @@ class TTSInferenceEngine(ReferenceLoader, VQManager):
                 response_queue=response_queue,
             )
         )
-
         return response_queue
-
     def get_audio_segment(self, result: GenerateResponse) -> np.ndarray:
         """
         Decode the VQ tokens to audio.
         """
-
         # Don't use autocast on MPS devices
         with autocast_exclude_mps(
             device_type=self.decoder_model.device.type, dtype=self.precision
         ):
             # Decode the symbolic tokens to audio
             segment = self.decode_vq_tokens(codes=result.codes)
-
         # Convert the audio to numpy
         return segment.float().cpu().numpy()
--- tools/download_models.py
+++ tools/download_models.py
@@ -1,8 +1,5 @@
 import os
-
 from huggingface_hub import hf_hub_download
-
-
 # Download
 def check_and_download_files(repo_id, file_list, local_dir):
     os.makedirs(local_dir, exist_ok=True)
@@ -19,12 +16,11 @@ def check_and_download_files(repo_id, file_list, local_dir):
             )
         else:
             print(f"{file} 已存在，跳过下载。")
-
-
 # 1st
 repo_id_1 = "fishaudio/fish-speech-1.5"
 local_dir_1 = "./checkpoints/fish-speech-1.5"
 files_1 = [
+    "gitattributes",
     ".gitattributes",
     "model.pth",
     "README.md",
@@ -33,7 +29,6 @@ files_1 = [
     "config.json",
     "firefly-gan-vq-fsq-8x1024-21hz-generator.pth",
 ]
-
 # 3rd
 repo_id_3 = "fishaudio/fish-speech-1"
 local_dir_3 = "./"
@@ -41,15 +36,12 @@ files_3 = [
     "ffmpeg.exe",
     "ffprobe.exe",
 ]
-
 # 4th
 repo_id_4 = "SpicyqSama007/fish-speech-packed"
 local_dir_4 = "./"
 files_4 = [
     "asr-label-win-x64.exe",
 ]
-
 check_and_download_files(repo_id_1, files_1, local_dir_1)
-
 check_and_download_files(repo_id_3, files_3, local_dir_3)
 check_and_download_files(repo_id_4, files_4, local_dir_4)
